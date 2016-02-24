e931a0714457a33fdc04ab0f3f92458580a983e5
hieubkcse <huynhtrunghieu.cs@gmail.com>
2016-02-24 17:42:56 +0700

    complete ex3 - Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,10 @@
+print "I will noew count my chicken:"
+print "A" , 25+30/6
+print "Hello", 1000 - 25 * 3 % 4
+print 3 +2 < 5-7
+print 1.00 + 2.33333
+print 1+2.33333
+print " 7 / 4", 7 / 4 
+print "7.00 / 4.00", 7.00 / 4.00 
+print " 7 % 4 = ", 7 % 4 
+print " 7.01 % 4.20 = ", 7.01 % 4.20 
