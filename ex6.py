8353fa7c9b07a905f20ec709b05dbcbe3749b4d5
Trung-Hieu Huynh <huynhtrunghieu.cs@gmail.com>
2016-02-27 13:49:32 +0700

    Complete ex6 - Strings and Text

---
+++ ex6.py
@@ -0,0 +1,16 @@
+x = "There are %s types of people." %10
+print x
+
+binary = "binary"
+do_not = "don't"
+
+y = "Those who know %s and those who %s" % (do_not, binary)
+print y
+
+print "I said: %r" %x
+print "I also said: '%s'." %y
+
+hilarious = False
+joke_evalution = "Isn't that joke so funny?! %s"
+
+print joke_evalution % hilarious
