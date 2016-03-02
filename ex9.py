e93ecce67f645c52209580cbb735ed9044bf9fd3
Trung-Hieu Huynh <huynhtrunghieu.cs@gmail.com>
2016-03-02 14:20:52 +0700

    Complete ex9 - Printing 3 times

---
+++ ex9.py
@@ -0,0 +1,10 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+		There's something going on here
+		Even 4 line.
+	""")
